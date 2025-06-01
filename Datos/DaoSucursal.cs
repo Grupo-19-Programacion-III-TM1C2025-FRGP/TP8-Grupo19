cef54f7f50b74a679e442fa6db6aaa202ea66588
sofianeduk <sofianeduk2013@gmail.com>
2025-06-01 11:00:29 -0300

    Carga gridview

---
+++ Datos/DaoSucursal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+   public class DaoSucursal
+    {
+        Conexion conexion = new Conexion();
+        public Sucursal getSucursal(Sucursal suc)
+        {
+            DataTable tabla = conexion.TraerTabla( "Select * from Sucursal where Id_Sucursal=" + suc.getIdSucursal(), "Sucursal");
+            suc.setIdSucursal(Convert.ToInt32(tabla.Rows[0][0].ToString()));
+            suc.setNombreSucursal(tabla.Rows[0][1].ToString());
+            suc.setDescripcionSucursal(tabla.Rows[0][2].ToString());
+            suc.setIdProvincia(Convert.ToInt32(tabla.Rows[0][3].ToString()));
+            suc.setDireccionSucursal(tabla.Rows[0][4].ToString());
+            return suc;
+        }
+
+        public DataTable getTablaSucursales()
+        {
+            DataTable tabla = conexion.TraerTabla("Select Id_Sucursal, NombreSucursal, DescripcionSucursal, Id_ProvinciaSucursal, DireccionSucursal from Sucursal", "Sucursal");
+            return tabla;
+        }
+
+    }
+}
---
+++ Negocio/NegocioSucursales.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Datos;
+using Entidades;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Negocio
+{
+    public class NegocioSucursales
+    {
+        public DataTable getTabla()
+        {
+            DaoSucursal dao = new DaoSucursal();
+            return dao.getTablaSucursales();
+        }
+
+        public Sucursal get(int id)
+        {
+            DaoSucursal dao = new DaoSucursal();
+            Sucursal sucursal = new Sucursal();
+            sucursal.setIdSucursal(id);
+            return dao.getSucursal(sucursal);
+        }
+    }
+}
